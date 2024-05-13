2bfd984898d61690243f9d590f4eb9d29010620b
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-13 06:28:50 +0500

    claim lar yozildi

--- MassUz.Application/UseCases/AuthService/AuthService.cs
+++ MassUz.Application/UseCases/AuthService/AuthService.cs
@@ -1,7 +1,12 @@
 ﻿using MassaUz.Domain.Entities.Auth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +14,28 @@ namespace MassUz.Application.UseCases.AuthService
 {
     public class AuthService : IAuthService
     {
-        public string GenerateToken(User user)
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
         }
 
-    }
+        public string GenerateToken(User user)
+        {
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTSettings:Secret"]!));
+            SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            int expirePeriod = int.Parse(_configuration["JWTSettings:Expire"]!);
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat,EpochTime.GetIntDate(DateTime.UtcNow).ToString(CultureInfo.InvariantCulture),ClaimValueTypes.Integer64),
+                new Claim(ClaimTypes.Name,user.Name!),
+                new Claim(ClaimTypes.Surname,user.Surname!),
+                new Claim(ClaimTypes.Role,user.Role!),
+            };
+        }
+
+    } 
 }
