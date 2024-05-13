7a0c064d6b1995ab364ed9f8f4768630037bd6b7
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-13 06:16:33 +0500

    adding IAuthService

--- MassaUz.Infrastructure/Persistance/MassaUzDbContext.cs
+++ MassaUz.Infrastructure/Persistance/MassaUzDbContext.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MassUz.Application.Abstractions;
+using MassaUz.Domain.Entities.Models;
+using MassaUz.Domain.Entities.Auth;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,47 @@ using System.Threading.Tasks;
 
 namespace MassaUz.Infrastructure.Persistance
 {
-    public class MassaUzDbContext
+    public class MassaUzDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>, IMassaUzDbContext
     {
+        public MassaUzDbContext(DbContextOptions<MassaUzDbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public Task<int> SaveChangeAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+
+            modelBuilder.Entity<Category>()
+                .HasData(
+                    [   new Category{ Id = 1, Name = "Оральные препараты"  },
+                        new Category{ Id = 2, Name = "Инъекционные препараты" },
+                        new Category{ Id = 3, Name = "Препараты ПКТ" },
+                        new Category{ Id = 4, Name = "Гормон роста" },
+                        new Category{ Id = 5, Name = "Сармы/Sarms" }
+                    ]
+                );
+        }
     }
+
+
+
 }
+
